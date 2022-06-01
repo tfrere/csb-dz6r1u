738963cca8162ed5f839e2f95c65d098341da91b
Code Sandbox <hello@codesandbox.io>
2022-06-01 01:14:17 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e232cab2f65fd7b69b171ad3adb408cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce2e60707c5acd7d3777f0a9a0a1e88",
+    "url": "https://tfrere.github.io/csb-dz6r1u/index.html"
+  },
+  {
+    "revision": "c89ba766ec45fc2ee610",
+    "url": "https://tfrere.github.io/csb-dz6r1u/static/css/main.ba2fce4e.chunk.css"
+  },
+  {
+    "revision": "3c59410e205fa4d2d225",
+    "url": "https://tfrere.github.io/csb-dz6r1u/static/js/2.d5905e08.chunk.js"
+  },
+  {
+    "revision": "c89ba766ec45fc2ee610",
+    "url": "https://tfrere.github.io/csb-dz6r1u/static/js/main.3d3becc0.chunk.js"
+  },
+  {
+    "revision": "b41079a5ffaa4f8dec3c",
+    "url": "https://tfrere.github.io/csb-dz6r1u/static/js/runtime~main.2a7e9b0e.js"
+  }
+]);
